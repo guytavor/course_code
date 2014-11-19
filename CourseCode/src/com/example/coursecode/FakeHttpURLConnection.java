b5dab58dbf348a0407502e2820b39b119bd6dfaa
Guy Tavor <guy@scoompa.com>
2014-11-19 11:45:19 +0200

    Initial import + FakeHttpURLConnection

---
+++ CourseCode/src/com/example/coursecode/FakeHttpURLConnection.java
@@ -0,0 +1,34 @@
+package com.example.coursecode;
+
+/**
+ * Fake HttpURLConnection for writing the cache exercise.
+ * 
+ * @author guyt
+ */
+public class FakeHttpURLConnection {
+
+	private String url;
+
+	private FakeHttpURLConnection(String url) {
+		this.url = url;
+	}
+	
+	public static FakeHttpURLConnection urlOpenConnection(String url) {
+		return new FakeHttpURLConnection(url);
+	}
+	
+	/**
+	 * For "first" url returns expiration of 5 seconds from now.
+	 */
+	public long getHeaderFieldDate(String field, long defaultValue) {
+		long now = System.currentTimeMillis();
+		final long inFiveSeconds = now + 5000;
+		if (url.equals("first")) {
+			if (field.equals("Expires")) {
+				return inFiveSeconds;
+			}			
+		}
+		return defaultValue;			
+	}
+	
+}
